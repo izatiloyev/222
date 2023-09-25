0bdb7d1cf40530d212c2c83a8cef7b8f577ecd1c
islom <kilhom606@gmail.com>
2023-09-25 14:58:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+let header = document.createElement('header')
+let divn = document.createElement('div')
+let a = document.createElement('p')
+let o = document.createElement('p')
+let pi = document.createElement('p')
+let pin = document.createElement('p')
+let ppp = document.createElement('img')
+let pins = document.createElement('p')
+let divns = document.createElement('div')
+let imgg = document.createElement('img')
+let w = document.createElement('p')
+let e = document.createElement('input')
+let ee = document.createElement('input')
+let r = document.createElement('p')
+let t = document.createElement('p')
+let y = document.createElement('p')
+let divnss = document.createElement('div')
+let s = document.createElement('p')
+let l = document.createElement('p')
+let k = document.createElement('p')
+let x = document.createElement('p')
+let c = document.createElement('p')
+let v = document.createElement('p')
+let d = document.createElement('p')
+let f = document.createElement('p')
+let g = document.createElement('p')
+let h = document.createElement('p')
+let j = document.createElement('p')
+let section = document.createElement('section')
+let di = document.createElement('div')
+let btn = document.createElement('button')
+let div = document.createElement('div')
+let img = document.createElement('img')
+let ll = document.createElement('p')
+let kk = document.createElement('p')
+let hh = document.createElement('h2')
+let rr = document.createElement('p')
+let vv = document.createElement('h1')
+
+                                                                                                  
+
+                                          
+
+
+
+
+
+divn.classList.add('one')
+divns.classList.add('two')
+w.classList.add('ret')
+e.classList.add('ttt')
+ee.classList.add('five')
+divnss.classList.add('three')
+di.classList.add('container')
+ll.classList.add('he')
+kk.classList.add('ff')
+hh.classList.add('le')
+rr.classList.add('ter')
+vv.classList.add ('ve')
+img.classList.add('rett')
+imgg.classList.add('wee')
+ppp.classList.add('ew')
+
+
+e.placeholder = "Каталог"
+ee.placeholder = "Искать товары и категории"
+ppp.src = "./img/ru.png"
+img.src = "./img/Remove-bg.ai_1695548206298.png"
+img.alt = "45850158"
+imgg.src = "./img/200x200bb.jpg"
+
+
+a.innerHTML = "Город:Ташкент"
+o.innerHTML = "Пункты выдачи"
+pi.innerHTML = "Доставки ваш заказ бесплатно -- всего за 1 день! "
+pin.innerHTML = "Вопрос-ответ     Мои заказы"
+pins.innerHTML = "Русский"
+w.innerHTML = "uzum market"
+r.innerHTML = "Войти"
+t.innerHTML = "Избранное"
+y.innerHTML = "Корзина"
+s.innerHTML = "Рассрочка"
+l.innerHTML = "Электроника"
+k.innerHTML = "Бытовая техника"
+c.innerHTML = "Одежда"
+v.innerHTML = "Обувь"
+d.innerHTML = "Аксессуары"
+f.innerHTML = "Красота"
+g.innerHTML = "Здоровье"
+h.innerHTML = "Товары для дома"
+j.innerHTML = "Строительство и ремонт"
+x.innerHTML = "Ещё"
+btn.innerHTML = "День рождения"
+ll.innerHTML = "Подсолнечное масло Щедрое лето,рафинированное,900мл"
+kk.innerHTML = "5.0 (1960 оценок)"
+hh.innerHTML = "1 680 сум/мес"
+rr.innerHTML = "21 000 сум"
+vv.innerHTML = "14 000 сум"
+
+
+let body = document.body
+
+
+body.append(header)
+header.append(divn, divns, divnss)
+divn.append(a, o, pi, pin, ppp, pins)
+divns.append(imgg, w, e, ee, r, t, y )
+divnss.append(s, l, k, c, v, d, f, g, h, j, x)
+body.append(section)
+section.append(di,div)
+di.append( img, btn )
+div.append(ll, kk, hh, rr, vv)
+
+
+
+
